f36ac89841aad3f6a416013de256ab01a0a04bd2
unknown <gditrani269@gmail.com>
2024-05-21 15:57:41 -0300

    Seccion 16 - 199. Añadiendo el Filtro JwtAuthentication en la

--- src/main/java/com/ger/backend/usersapp/backendusersapp/auth/SpringSecurityConfiguration.java
+++ src/main/java/com/ger/backend/usersapp/backendusersapp/auth/SpringSecurityConfiguration.java
@@ -1,21 +1,30 @@
 package com.ger.backend.usersapp.backendusersapp.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.ger.backend.usersapp.backendusersapp.auth.filters.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfiguration {
     //aca configuramos nuestra seguridad
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
         .requestMatchers(HttpMethod.GET, "/users").permitAll()
         .anyRequest().authenticated()
-        .and()
+                .and()
+        .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
         .csrf(config -> config.disable())
         .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
