39653b398a9c038ac9b1a00794a5dd8d0a20df12
unknown <gditrani269@gmail.com>
2024-06-24 14:46:52 -0300

    Seccion 16 - 229. Patron DTO Mapper Builder Optimizado

--- src/main/java/com/ger/backend/usersapp/backendusersapp/models/dto/mapper/DtoMapperUser.java
+++ src/main/java/com/ger/backend/usersapp/backendusersapp/models/dto/mapper/DtoMapperUser.java
@@ -7,7 +7,8 @@ import com.ger.backend.usersapp.backendusersapp.models.entities.User;
 
 public class DtoMapperUser {
 
-    private static DtoMapperUser mapper;
+    //static DtoMapperUser mapper esto podria traer conflicto por ser una variable estatica cuando haya mas usuario, porque al ser estatica se usa en toda la aplcacion.
+    //private static DtoMapperUser mapper;
 
     private User user;
     private DtoMapperUser () {
@@ -15,13 +16,14 @@ public class DtoMapperUser {
     }
 
     public static DtoMapperUser builder () {
-        mapper = new DtoMapperUser ();
-        return mapper;
+        //aca tambien cambiamos, simplemente devolvemos la instancia para salvar el problema del static de 
+        //static DtoMapperUser mapper, asi cada instancia es unica por cada usuario
+        return new DtoMapperUser ();
     }
 
     public DtoMapperUser setUser(User user) {
         this.user = user;
-        return mapper;
+        return this;
     }
 
     public UserDto build () {
