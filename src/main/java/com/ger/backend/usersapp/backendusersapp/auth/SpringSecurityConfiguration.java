0e2f5f9e40e8ab7747e8dd3201884b58d38b616d
unknown <gditrani269@gmail.com>
2024-06-25 15:18:54 -0300

    Seccion 17 - 232. Configurando Cors de Spring Security

--- src/main/java/com/ger/backend/usersapp/backendusersapp/auth/SpringSecurityConfiguration.java
+++ src/main/java/com/ger/backend/usersapp/backendusersapp/auth/SpringSecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.ger.backend.usersapp.backendusersapp.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.ger.backend.usersapp.backendusersapp.auth.filters.JwtAuthenticationFilter;
 import com.ger.backend.usersapp.backendusersapp.auth.filters.JwtValidationFilter;
@@ -49,7 +57,29 @@ public class SpringSecurityConfiguration {
                 .addFilter(new JwtValidationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(config -> config.disable())
                 .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors (cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource () {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8082"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter () {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
